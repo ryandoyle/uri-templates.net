26836c35bbd2bda55caf9b9fe29714c61afa3086
Ryan Doyle <ryan@doylenet.net>
2020-05-07 20:30:12 +1000

    Updates

---
+++ precache-manifest.bef9d0aa44c021bc5b4c3a196d3b6717.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a7d0584b98478bde15414a9cdd0db1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd50de0340b874057e65",
+    "url": "/static/css/2.ae87030c.chunk.css"
+  },
+  {
+    "revision": "5d5da53e5b4d0daa3c8d",
+    "url": "/static/css/main.2b6bd002.chunk.css"
+  },
+  {
+    "revision": "dd50de0340b874057e65",
+    "url": "/static/js/2.f276548b.chunk.js"
+  },
+  {
+    "revision": "8dea9a823bfb24207fef47cb32cf30ad",
+    "url": "/static/js/2.f276548b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d5da53e5b4d0daa3c8d",
+    "url": "/static/js/main.00cdf58e.chunk.js"
+  },
+  {
+    "revision": "d4e02eb7e74603308a3a",
+    "url": "/static/js/runtime-main.32e3ad1c.js"
+  }
+]);
