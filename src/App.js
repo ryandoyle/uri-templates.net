f9f751c1a09c45b4ab27d7d8e9d21d54093fb2d0
Ryan Doyle <ryan@doylenet.net>
2020-05-10 10:28:18 +1000

    Add logo

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import React from 'react';
 import './App.css';
 import {UriTemplater} from "./UriTemplater";
 import Container from "@material-ui/core/Container";
+import logo from './logo192.png';
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 
 function App() {
   return (
-    <div className="App">
-        <Container maxWidth={"md"}>
-            <h3>URI Templates.net</h3>
-            <UriTemplater />
-        </Container>
+    <div className="App" >
+        <Box mt={6}>
+            <Container maxWidth={"md"}>
+                <Grid container direction={"row"} justify={"center"} alignItems={"center"} spacing={2}>
+                    <Grid item>
+                        <img src={logo} alt={"What Controls Logo"}/>
+                    </Grid>
+                    <Grid item>
+                        <Typography variant="h4" gutterBottom>
+                            URI TEMPLATES.NET
+                        </Typography>
+                    </Grid>
+                </Grid>
+
+
+                <UriTemplater />
+            </Container>
+        </Box>
     </div>
   );
 }
