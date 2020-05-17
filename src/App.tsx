3cc3c0010308703d4ab6ab3373fd39d9841e9123
Ryan Doyle <ryan@doylenet.net>
2020-05-17 15:23:23 +1000

    Add about section

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+import Link from "@material-ui/core/Link";
 const logo = require('./logo192.png');
 
 function App() {
@@ -25,6 +26,16 @@ function App() {
 
                 <UriTemplater />
             </Container>
+        </Box>
+        <Box position="fixed" p={1} style={{top: "auto", bottom: 0, width: '100%'}}>
+            <Link
+                href={"https://github.com/ryandoyle/uri-templates.net"}
+                target="_blank"
+                color={"textSecondary"}
+                variant={"button"}
+            >
+                About
+            </Link>
         </Box>
     </div>
   );
